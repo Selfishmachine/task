ed117bba085ad573f9236b030df32f7e711e5398
haider sadoon <91218301+Selfishmachine@users.noreply.github.com>
2022-04-07 04:50:59 +0300

    Add files via upload

---
+++ New folder/Q1-Dart.dart
@@ -0,0 +1,13 @@
+main() {
+  const hour = DataTime().hour;
+  print(hour);
+}
+
+//the proplem is that we cant use const cuz the time is changing and we can use 
+//const on the compile time like const x=2;
+
+
+// main(){
+//   var hour = DateTime.now().hour;
+//   print(hour);
+// }
